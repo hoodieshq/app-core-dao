fd9a7f961840ace2e80b66ead3f77dad9c47ccf1
Roman Maslennikov <kafkapro@gmail.com>
2025-06-27 00:49:48 +0300

    Update UI flow V2

--- src/display.c
+++ src/display.c
@@ -7,6 +7,8 @@
 #include "nbgl_use_case.h"
 #include "time_helper.h"
 
+#define MAX_N_PAIRS 9
+
 static void review_choice(bool approved) {
     set_ux_flow_response(approved);  // sets the return value of io_ui_process
 
@@ -18,7 +20,55 @@ static void review_choice(bool approved) {
     }
 }
 
-#define MAX_N_PAIRS 9
+#ifdef SCREEN_SIZE_WALLET
+
+static int review_title(tx_type_t type, char *out_buf, size_t out_buf_len) {
+    const char *review_title = NULL;
+
+    if (type & TYPE_TX_LOCK) {
+        review_title = "Review transaction to lock BTC to earn CORE";
+    } else {
+        review_title = "Review transaction to unlock BTC";
+    }
+
+    return snprintf(out_buf, out_buf_len, "%s", review_title);
+}
+
+static int sign_title(tx_type_t type, char *out_buf, size_t out_buf_len) {
+    const char *review_title = NULL;
+
+    if (type & TYPE_TX_LOCK) {
+        review_title = "Sign transaction to lock BTC to earn CORE?";
+    } else {
+        review_title = "Sign transaction to unlock BTC?";
+    }
+
+    return snprintf(out_buf, out_buf_len, "%s", review_title);
+}
+
+#else
+
+static const char *tx_type_to_str(tx_type_t type) {
+    if (type & TYPE_TX_LOCK) {
+        return "lock";
+    } else {
+        return "unlock";
+    }
+}
+
+static int review_title(tx_type_t type, char *out_buf, size_t out_buf_len) {
+    const char *operation_txt = tx_type_to_str(type);
+
+    return snprintf(out_buf, out_buf_len, "Review transaction to %s BTC", operation_txt);
+}
+
+static int sign_title(tx_type_t type, char *out_buf, size_t out_buf_len) {
+    (void) type;
+
+    return snprintf(out_buf, out_buf_len, "Sign transaction");
+}
+
+#endif
 
 bool display_transaction(dispatcher_context_t *dc,
                          int64_t value_spent,
@@ -29,17 +79,16 @@ bool display_transaction(dispatcher_context_t *dc,
 
     // Format values
     char *chain_id;
-    char *operation_type = NULL;
 
-    char review_title[36] = {0};
-    char sign_title[36] = {0};
+    char review_title_str[46] = {0};
+    char sign_title_str[46] = {0};
     char value_str[32] = {0};
     char fee_str[32] = {0};
     char unstake_value_str[32] = {0};
     char delegator_str[43] = {0};
     char validator_str[43] = {0};
     char locktime_str[DATETIME_STR_LEN] = {0};
-    char core_fee_str[4] = {0};
+    char core_fee_str[10] = {0};
     uint64_t value_spent_abs = value_spent < 0 ? -value_spent : value_spent;
     format_sats_amount(COIN_COINID_SHORT, value_spent_abs, value_str);
     format_sats_amount(COIN_COINID_SHORT, fee, fee_str);
@@ -47,7 +96,7 @@ bool display_transaction(dispatcher_context_t *dc,
     format_address(info->delegator, 20, delegator_str, sizeof(delegator_str));
     format_address(info->validator, 20, validator_str, sizeof(validator_str));
     timestamp_to_string(info->locktime, locktime_str);
-    snprintf(core_fee_str, 4, "%d", info->fee);
+    snprintf(core_fee_str, sizeof(core_fee_str), "%d CORE", info->fee);
 
     if (info->chain_id == CHAID_ID_MAINNET) {
         chain_id = (char *) "Mainnet";
@@ -66,29 +115,26 @@ bool display_transaction(dispatcher_context_t *dc,
 #endif
     }
 
-    if (info->type & TYPE_TX_LOCK && info->type & TYPE_TX_UNLOCK) {
-        operation_type = (char *) "restake";
-    } else if (info->type & TYPE_TX_LOCK) {
-        operation_type = (char *) "stake";
-    } else if (info->type & TYPE_TX_UNLOCK) {
-        operation_type = (char *) "unstake";
-    }
-
-    snprintf(review_title, sizeof(review_title), "Review transaction to\n %s BTC", operation_type);
-    snprintf(sign_title, sizeof(sign_title), "Sign transaction to\n %s BTC?", operation_type);
+    review_title(info->type, review_title_str, sizeof(review_title_str));
+    sign_title(info->type, sign_title_str, sizeof(sign_title_str));
 
     int n_pairs = 0;
 
     if (info->type & TYPE_TX_LOCK) {
         pairs[n_pairs++] = (nbgl_layoutTagValue_t){
-            .item = "Stake amount",
+            .item = "Lock amount",
             .value = value_str,
+        };
+
+        pairs[n_pairs++] = (nbgl_layoutTagValue_t){
+            .item = "Lock until (UTC)",
+            .value = locktime_str,
         };
     }
 
-    if (info->type & TYPE_TX_UNLOCK) {
+    if (info->type == TYPE_TX_UNLOCK) {
         pairs[n_pairs++] = (nbgl_layoutTagValue_t){
-            .item = "Unstake amount",
+            .item = "Unlock amount",
             .value = unstake_value_str,
         };
     }
@@ -99,16 +145,9 @@ bool display_transaction(dispatcher_context_t *dc,
             .value = delegator_str,
         };
 
-        pairs[n_pairs++] = (nbgl_layoutTagValue_t){
-            .item = "Validator",
-            .value = validator_str,
-            .forcePageStart = true,
-        };
-
-        pairs[n_pairs++] = (nbgl_layoutTagValue_t){
-            .item = "Locktime (UTC)",
-            .value = locktime_str,
-        };
+        pairs[n_pairs++] = (nbgl_layoutTagValue_t){.item = "Validator",
+                                                   .value = validator_str,
+                                                   .forcePageStart = true};
 
         pairs[n_pairs++] = (nbgl_layoutTagValue_t){.item = "CORE fee", .value = core_fee_str};
     }
@@ -116,14 +155,12 @@ bool display_transaction(dispatcher_context_t *dc,
     pairs[n_pairs++] = (nbgl_layoutTagValue_t){
         .item = "Fees",
         .value = fee_str,
-        .forcePageStart = info->type != TYPE_TX_UNLOCK,
     };
 
     if (info->chain_id != CHAID_ID_MAINNET) {
-        pairs[n_pairs++] = (nbgl_layoutTagValue_t){
-            .item = "Network",
-            .value = chain_id,
-        };
+        pairs[n_pairs++] = (nbgl_layoutTagValue_t){.item = "Network",
+                                                   .value = chain_id,
+                                                   .forcePageStart = TYPE_TX_UNLOCK != info->type};
     }
 
     assert(n_pairs <= MAX_N_PAIRS);
@@ -136,9 +173,9 @@ bool display_transaction(dispatcher_context_t *dc,
     nbgl_useCaseReview(TYPE_TRANSACTION,
                        &pairList,
                        &ICON_APP_HOME,
-                       review_title,
+                       review_title_str,
                        NULL,
-                       sign_title,
+                       sign_title_str,
                        review_choice);
 
     // blocking call until the user approves or rejects the transaction
